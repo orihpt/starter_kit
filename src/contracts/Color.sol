a224b0285c08ab907cbef0f820d3b953088fb17e
orihpt <ori9070.hpt@gmail.com>
2021-04-15 16:09:03 +0300

    1:01:40

--- src/contracts/Color.sol
+++ src/contracts/Color.sol
@@ -3,17 +3,18 @@ pragma solidity 0.5.0;
 import "./ERC721Full.sol";
 
 contract Color is ERC721Full {
+
+	string[] public colors;
+	mapping(string => bool) _colorExists; // Dictionary
 	
 	constructor() ERC721Full("Color", "COLOR") public {
 	}
 
 	function mint(string memory _color) public {
 		// Require unique color
-
-		// Color - dd it
-
-		// Call the mint function
-
-		// Color - track it
+		require(!_colorExists[_color]);
+		uint _id = colors.push(_color); // Append
+		_mint(msg.sender, _id);
+		_colorExists[_color] = true;
 	}
 }
--- test/Color.test.js
+++ test/Color.test.js
@@ -31,4 +31,42 @@
 			assert.equal(symbol, 'COLOR')
 		})
 	})
+
+	describe('minting', async() => {
+
+		it('creates a new token', async() => {
+			const result = await contract.mint('#EC058E')
+			const totalSupply = await contract.totalSupply()
+			// SUCCESS
+			assert.equal(totalSupply, 1)
+			console.log(result)
+			const event = result.logs[0].args
+			assert.equal(event.tokenId.toNumber(), 1, 'id is correct')
+			assert.equal(event.from, '0x0000000000000000000000000000000000000000', 'from is correct')
+			assert.equal(event.to, accounts[0], 'to is correct')
+
+			// FALIURE
+			await contract.mint('#EC058E').should.be.rejected;
+		})
+	})
+
+	describe('indexing', async() => {
+		it('lists colors', async() => {
+			// Mint 3 צםרק tokens
+			await contract.mint('#5386E4')
+			await contract.mint('#FFFFFF')
+			await contract.mint('#000000')
+			const totalSupply = await contract.totalSupply()
+
+			let color
+			let result = []
+			for (var i = 1; i <= totalSupply; i++) {
+				color = await contract.colors(i - 1)
+				result.push(color)
+			}
+
+			let expected = ['#EC058E', '#5386E4', '#FFFFFF', '#000000']
+			assert.equal(result.join(','), expected.join(','))
+		})
+	})
 })
