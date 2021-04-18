07fc3bb3f60ff2c018a4f4d5bf0f44882458390d
orihpt <ori9070.hpt@gmail.com>
2021-04-18 10:33:15 +0300

    1:18:49

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
+import Web3 from 'web3'
 import './App.css';
 
 class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
 
 async loadWeb3() {
   if (window.ethereum) {
@@ -17,42 +22,47 @@ async loadWeb3() {
     }
 }
 
+async loadBlockchainData() {
+  const web3 = window.web3
+
+  // Load account
+  const accounts = await web3.eth.getAccounts()
+  this.setState({ account: accounts[0] })
+}
+
+constructor(props) {
+  super(props)
+  this.state = {
+    account: ''
+  }
+}
+
+
   render() {
     return (
       <div>
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiL0NuDroDwAhUy_rsIHUFwCZgQ3ywwAHoECAQQAg&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DdQw4w9WgXcQ&usg=AOvVaw0aHtehaphMhOCAkCydRLZU"
+            href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
             target="_blank"
             rel="noopener noreferrer"
           >
             Colors Tokens
           </a>
+          <ul className="navbar-nav px-3">
+           <li className = "nav-item text-nowrap d-none d-sm-none d-sm-block">
+            <small className="text-white"><span id="account">
+              {this.state.account}
+            </span></small>
+            </li>
+          </ul>
         </nav>
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+
               </div>
             </main>
           </div>
